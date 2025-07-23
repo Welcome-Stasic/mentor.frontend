d80ad0d71732c70f2d91e9f11ba8f47220be7c68
12bsg12 <carlj8335@gmail.com>
2025-07-23 13:05:56 +0500

    fix

--- app/Account/Register/layout.tsx
+++ app/Account/Register/layout.tsx
@@ -1,9 +1,27 @@
 import { SITE_BASE_NAME } from "@/constants";
 import { Metadata } from "next";
 
-export const metadata: Metadata  = {
+const pageUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/Account/Register`;
+
+export const metadata: Metadata = {
   title: `Регистрация | ${SITE_BASE_NAME}`,
-  description: `Регистрация пользователя в системе ${SITE_BASE_NAME}`,
+  description: `Создание нового аккаунта в системе ${SITE_BASE_NAME}`,
+  keywords: ['регистрация', 'создать аккаунт', 'новый пользователь', SITE_BASE_NAME],
+  robots: {
+    index: false,
+    follow: false,
+  },
+  alternates: {
+    canonical: pageUrl,
+  },
+  openGraph: {
+    title: `Регистрация | ${SITE_BASE_NAME}`,
+    description: `Создание нового аккаунта в системе ${SITE_BASE_NAME}`,
+    url: pageUrl,
+    siteName: SITE_BASE_NAME,
+    locale: 'ru_RU',
+    type: 'website',
+  },
 };
 
 export default function RegisterLayout({ children }: { children: React.ReactNode }) {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,31 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Providers from '@/components/Providers';
+import { SITE_BASE_NAME } from '@/constants';
+
+const pageUrl = `${process.env.NEXT_PUBLIC_SITE_URL}`;
 
 export const metadata: Metadata = {
-  title: 'Главная',
   icons: {
     icon: '/favicon.ico',
+  },
+  title: `Главная | ${SITE_BASE_NAME}`,
+  description: `Добро пожаловать на главную страницу ${SITE_BASE_NAME}`,
+  keywords: ['главная', SITE_BASE_NAME, 'эрис'],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: pageUrl,
+  },
+  openGraph: {
+    title: `Главная | ${SITE_BASE_NAME}`,
+    description: `Добро пожаловать на главную страницу ${SITE_BASE_NAME}`,
+    url: pageUrl,
+    siteName: SITE_BASE_NAME,
+    locale: 'ru_RU',
+    type: 'website',
   },
 };
 
