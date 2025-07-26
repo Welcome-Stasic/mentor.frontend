94f0427c16995612c226b7f2a55f967c671ce1e4
BSG <carlj8335@gmail.com>
2025-07-27 01:01:32 +0500

    fix

--- app/(dashboard)/TimeTracking/[slug]/page.tsx
+++ app/(dashboard)/TimeTracking/[slug]/page.tsx
@@ -1,7 +1,11 @@
-// ✅ Родительская страница
 import TimeContainer from '@/components/Time/TimeContainer';
 import { createLoader, parseAsString } from 'nuqs/server';
 import type { SearchParams } from 'nuqs/server';
+
+interface IPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<SearchParams>;
+}
 
 const pageSearchParams = {
   dateIn: parseAsString.withDefault(''),
@@ -10,14 +14,8 @@ const pageSearchParams = {
 
 const loadSearchParams = createLoader(pageSearchParams);
 
-export default async function TimePage({
-  params,
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams: SearchParams;
-}) {
-  const { slug: userId } = params;
+export default async function TimePage({ params, searchParams }: IPageProps) {
+  const { slug: userId } = await params;
   const { dateIn, dateOut } = await loadSearchParams(searchParams);
 
   return <TimeContainer userId={userId} dateIn={dateIn} dateOut={dateOut || dateIn} />;
