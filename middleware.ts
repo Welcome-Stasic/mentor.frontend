2aa428f2639daa2efa419b6fc55222178bc65008
12bsg12 <carlj8335@gmail.com>
2025-07-28 15:20:12 +0500

    fix

--- middleware.ts
+++ middleware.ts
@@ -24,6 +24,6 @@ export default withAuth(middleware, {
 // 🔐 Указываем исключения: разрешён доступ к этим маршрутам без авторизации
 export const config = {
   matcher: [
-    '/((?!_next/static|favicon.ico|api|Account/Login|Account/Register|Account/ResetPassword|EmailConfirmed|not-found|privacy-policy).*)',
+    '/((?!_next/static|favicon.ico|api|backend|Account/Login|Account/Register|Account/ResetPassword|EmailConfirmed|not-found|privacy-policy).*)',
   ],
 };
