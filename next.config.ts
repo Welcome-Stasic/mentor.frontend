bd61ec288c2586c5bac9ec385ece273db91b8ff9
GlushenkovVV <GlushenkovVV@PC100001>
2025-11-28 11:21:37 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['randomuser.me', 'eriskip.com', 'selstorage.ru', 'avatars.yandex.net', 'lh3.googleusercontent.com'],
   },
-  output: process.env.NEXT_OUTPUT === 'standalone' ? 'standalone' : undefined,
+  output: 'standalone',
 };
 
 export default nextConfig;
