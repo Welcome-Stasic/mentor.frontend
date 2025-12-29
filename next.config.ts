91ca5d2bc71687bfa368780dcba61496c7c4e4fb
GlushenkovVV <GlushenkovVV@PC100001>
2025-12-29 10:21:32 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,31 +6,6 @@ const nextConfig: NextConfig = {
   },
   output: 'standalone',
   productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          }
-        ]
-      }
-    ]
-  }
 };
 
 export default nextConfig;
